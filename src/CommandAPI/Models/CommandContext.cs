59046ed30e29793c664a66afeea3e3130de97fbd
Ricardo Griffith <ricardo.griffith@icloud.com>
2020-09-17 10:15:37 -0300

    [add] DBContext to allow Linq  --> PostgreSQL.

---
+++ src/CommandAPI/Models/CommandContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CommandAPI.Models
+{
+    /// <summary>
+    /// This means that we can choose which classes, (model classes), 
+    /// we want to put under DBContext control and hence represent 
+    /// in the DB.
+    /// </summary>
+    public class CommandContext : DbContext
+    {
+        public CommandContext(DbContextOptions<CommandContext> options) : base(options)
+        { }
+        public DbSet<Command> CommandItems { get; set; }
+    }
+}
