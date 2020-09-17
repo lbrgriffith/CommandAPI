45b2dc314b653c70b6ba4aa1caf99bc88c4ffe5b
Ricardo Griffith <ricardo.griffith@icloud.com>
2020-09-17 07:20:04 -0300

    Initial commit

---
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CommandsController : ControllerBase 
+{
+    [HttpGet] 
+    public ActionResult<IEnumerable<string>> Get() 
+    { 
+        return new string[] 
+        { "this", "is", "hard", "coded" }; 
+    } 
+}
