677fb4c031d27ab52458e87a2b4703306be1ad1f
Ricardo Griffith <ricardo.griffith@icloud.com>
2020-09-18 15:34:06 -0300

    [add] method to return items from database.

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -1,13 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CommandAPI.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Configuration;
 
 namespace CommandAPI
 {
@@ -20,7 +17,10 @@ namespace CommandAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddDbContext<CommandContext>(opt => 
+            opt.UseNpgsql(Configuration.GetConnectionString("PostgreSqlConnection")));
+
+                    services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
